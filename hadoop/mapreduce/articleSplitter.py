edd32bd4ce0713f557b75782f71c7e81e80b47fe
Diego Calvo <calvod@umich.edu>
2016-04-07 23:52:20 -0400

    article splitter works for real

--- hadoop/mapreduce/articleSplitter.py
+++ hadoop/mapreduce/articleSplitter.py
@@ -2,7 +2,7 @@ import xml.etree.ElementTree as ET
 import os
 import sys
 
-treeArticles = ET.parse('miningArticlesExample.xml')
+treeArticles = ET.parse('../../data/mining.articles.xml')
 articles = treeArticles.getroot()
 
 ticker = 0
@@ -13,12 +13,15 @@ file = open('splitArticles/articleFile'+str(ticker), 'w')
 for article in articles:
 	file.write("<"+article.tag+">\n")
 	for articleAttribute in article:
-		file.write("\t<"+articleAttribute.tag+">"+articleAttribute.text+"</"+articleAttribute.tag+">\n")
+		file.write("\t<"+articleAttribute.tag+">")
+		if articleAttribute.text:
+			file.write(articleAttribute.text)
+		file.write("</"+articleAttribute.tag+">\n")
 	spotWithinFile += 1
 	file.write("\n</"+article.tag+">\n")
 	if spotWithinFile == articlesPerFile:
 		ticker += 1
-		print "lol"
+		print ticker
 		spotWithinFile = 0
 		file = open('splitArticles/articleFile'+str(ticker), 'w')
 	

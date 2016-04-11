7108b90ca180e3a49a59304454dac2cb04b18e42
Nick Cruz <ncruz@umich.edu>
2016-04-11 18:25:41 -0400

    hadoop vs command line both output the same data

--- hadoop/run.sh
+++ hadoop/run.sh
@@ -3,6 +3,7 @@
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle
 
 # Hadoop doesn't like to clobber
+rm -rf mapreduce/intermediate
 rm -rf mapreduce/output
 
 # Job 1
@@ -11,16 +12,16 @@ rm -rf mapreduce/output
   -D mapreduce.job.maps=4 \
   -D mapreduce.job.reduces=1 \
   -input ./mapreduce/input \
-  -output ./mapreduce/output \
-  -mapper ./mapreduce/map.py \
-  -reducer ./mapreduce/reduce.py
+  -output ./mapreduce/intermediate \
+  -mapper ./mapreduce/ourMapper1.py \
+  -reducer ./mapreduce/ourReducer1.py
 
 # Job 2
 ./bin/hadoop \
   jar ./share/hadoop/tools/lib/hadoop-streaming-2.7.2.jar \
   -D mapreduce.job.maps=1 \
   -D mapreduce.job.reduces=1 \
-  -input ./mapreduce/input \
+  -input ./mapreduce/intermediate \
   -output ./mapreduce/output \
-  -mapper ./mapreduce/map.py \
-  -reducer ./mapreduce/reduce.py
+  -mapper ./mapreduce/ourMapper2.py \
+  -reducer ./mapreduce/ourReducer2.py

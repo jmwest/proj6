98f58fb31aeafd7907792a65b847a383483d49fe
Nick Cruz <ncruz@umich.edu>
2016-04-12 19:12:28 -0400

    rix

--- hadoop/mapreduce/ourMapper0.py
+++ hadoop/mapreduce/ourMapper0.py
@@ -4,12 +4,28 @@
 import sys
 
 docCount = 0
+ticker = 0
+
+wordDict = {}
 
 for line in sys.stdin:
-	try:
+	if (ticker % 3 == 0):
 		docId = int(line)
 		docCount += 1
-	except:
-		pass
+
+	# elif (ticker % 3 == 2):
+	# 	line_array = line.split()
+
+	# 	for word in line_array:
+	# 		if word in wordDict:
+	# 			wordDict[word] += 1
+	# 		else:
+	# 			wordDict[word] = 1
+
+	ticker += 1
+
+# for word in wordDict:
+# 	with open('mapreduce/n_k/' + word, 'w') as myfile:
+# 		myfile.write(str(wordDict[word]) + '\n')
 
 print docCount

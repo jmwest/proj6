1d0bd6a4ac1d2413230b126d67b7d26d3414da03
Nick Cruz <ncruz@umich.edu>
2016-04-14 15:12:22 -0400

    update catparts a little

--- hadoop/catparts.sh
+++ hadoop/catparts.sh
@@ -2,7 +2,7 @@ cd mapreduce
 
 # cat intermediate/part-* | sort > intermediate.txt
 # cat intermediate2/part-* | sort > intermediate2.txt
-cat output/part-* | sort > ../index_server/inverted_index.txt
+cat output/part-* | sort > ../../index_server/inverted_index.txt
 
 # cat test_intermediate/part-* | sort > test_intermediate/intermediate.txt
 # cat test_output/part-* | sort > test_output/output.txt
